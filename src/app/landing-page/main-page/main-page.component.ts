1a94769fed3b87b77cdfd5b96529a4c75c5f0f49
Marvin Schneemann <marvinschneemann@gmx.de>
2025-03-29 22:50:32 +0100

    Update Navbar

--- src/app/landing-page/main-page/main-page.component.ts
+++ src/app/landing-page/main-page/main-page.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
-import { NavbarComponent } from "./navbar/navbar.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { AbilitiesComponent } from "./abilities/abilities.component";
 import { MyProjectsComponent } from "./my-projects/my-projects.component";
 import { ContactMeComponent } from "./contact-me/contact-me.component";
+import { NavbarComponent } from './navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [AboveTheFoldComponent, NavbarComponent, AboutMeComponent, AbilitiesComponent, MyProjectsComponent, ContactMeComponent,CommonModule],
+  imports: [AboveTheFoldComponent, AboutMeComponent, AbilitiesComponent, MyProjectsComponent, ContactMeComponent, NavbarComponent, CommonModule ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
--- src/app/landing-page/main-page/navbar/navbar.component.ts
+++ src/app/landing-page/main-page/navbar/navbar.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 
 export class NavbarComponent {
+  private lastScrollTop = 0;
+  private navbarOffset = 683;
+  isFixed = false;
+  isOriginal = true;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop > this.navbarOffset) {
+      this.isFixed = true;
+      this.isOriginal = false;
+    } else {
+      this.isFixed = false;
+      this.isOriginal = true;
+    }
+    this.lastScrollTop = scrollTop;
+  }
 }
