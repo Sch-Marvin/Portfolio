4244d3dea2ba0cd47af55efc7cacf5f86f758f33
Marvin Schneemann <marvinschneemann@gmx.de>
2025-05-15 05:44:43 +0200

    added contact

--- src/app/landing-page/main-page/contact-me/contact-me.component.ts
+++ src/app/landing-page/main-page/contact-me/contact-me.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule,} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
+
+contactData = {
+
+  name: "",
+  email: "",
+  message: "",
+}
+    
+
+
+  onSubmit() {
+    console.log(this.contactData);
+  }
+
 
 }
