04c7ef4f8c199f29f755680d31615b4303166660
Marvin Schneemann <marvinschneemann@gmx.de>
2025-07-25 11:54:30 +0200

    responsive - Footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,16 +1,45 @@
 import { Component } from '@angular/core';
-import { TranslateModule, TranslateService } from '@ngx-translate/core'; // 👈 Import hinzufügen
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+  
 
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [CommonModule, TranslateModule],  // <- Wichtig für *ngFor
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  contactIcons = [
+    {
+      id: 'email',
+      default: '../../../../assets/icon/Footer/Email-button.svg',
+      hover: '../../../../assets/icon/Footer/Email-button-hover.svg',
+      current: '../../../../assets/icon/Footer/Email-button.svg',
+    },
+    {
+      id: 'github',
+      default: '../../../../assets/icon/Footer/Github button.svg',
+      hover: '../../../../assets/icon/Footer/Github button-hover.svg',
+      current: '../../../../assets/icon/Footer/Github button.svg',
+    },
+    {
+      id: 'linkedin',
+      default: '../../../../assets/icon/Footer/Linkedin button.svg',
+      hover: '../../../../assets/icon/Footer/Linkedin button-hover.svg',
+      current: '../../../../assets/icon/Footer/Linkedin button.svg',
+    }
+  ];
 
+  onContactHover(id: string) {
+    const icon = this.contactIcons.find(i => i.id === id);
+    if (icon) icon.current = icon.hover;
+  }
 
-  
+  onContactLeave(id: string) {
+    const icon = this.contactIcons.find(i => i.id === id);
+    if (icon) icon.current = icon.default;
+  }
 }
